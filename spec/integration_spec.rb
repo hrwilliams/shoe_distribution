0c4a3d29ff1702a271a9f8011a9db386573c2c7a
Halle Williams <williams.hr@gmail.com>
2016-07-31 16:07:18 -0700

    add more integration specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -30,3 +30,51 @@ describe('brand functionality through application', {:type => :feature}) do
     expect(page).to have_content('COOL SHOES BRO')
   end
 end
+
+describe('store functionality through application', {:type => :feature}) do
+  it('allows a user to add a new store') do
+    visit('/')
+    click_link('View Store')
+    fill_in('store_name', :with => "Cool Shoes Bro")
+    click_button('Add Store')
+    expect(page).to have_content('COOL SHOES BRO')
+  end
+
+  it('allows a user to view details for a single store') do
+    visit('/stores')
+    fill_in('store_name', :with => "Cool Shoes Bro")
+    click_button('Add Store')
+    click_link("COOL SHOES BRO")
+    expect(page).to have_content("COOL SHOES BRO")
+  end
+
+  it('allows a user to associate a brand with a store') do
+    visit('/brands')
+    fill_in('brand_name', :with => "Shoez")
+    click_button('Add Brand')
+    click_link('View Stores')
+    fill_in('store_name', :with => "Cool Shoes Bro")
+    click_button('Add Store')
+    click_link("COOL SHOES BRO")
+    select("SHOEZ", :from => "brand_id")
+    click_button("Submit")
+    expect(page).to have_content("SHOEZ")
+  end
+
+  it('allows the user to delete a store') do
+    Store.create({:name => "Cool Shoes Bro"})
+    visit('/stores')
+    click_link ('here')
+    click_button ('Delete Store')
+    expect(page).to have_no_content('COOL SHOES BRO')
+  end
+
+  it('allows the user to edit a shoe stores name') do
+    Store.create({:name => "Cool Shoes Bro"})
+    visit('/stores')
+    click_link ('here')
+    fill_in('name', :with => "Shoe Store")
+    click_button ('Update')
+    expect(page).to have_content('SHOE STORE')
+ end
+end
